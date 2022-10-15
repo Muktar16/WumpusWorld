2e28d2e474b33b6863a6da9ac854941ba7452374
Muktar16 <48786734+Muktar16@users.noreply.github.com>
2022-10-15 08:02:42 +0600

    Delete wumpus-world-frontend directory

--- wumpus-world-frontend/src/app/cell-visited.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CellVisited } from './cell-visited';
-
-describe('CellVisited', () => {
-  it('should create an instance', () => {
-    expect(new CellVisited()).toBeTruthy();
-  });
-});
--- wumpus-world-frontend/src/app/wumpus.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Wumpus } from './wumpus';
-
-describe('Wumpus', () => {
-  it('should create an instance', () => {
-    expect(new Wumpus()).toBeTruthy();
-  });
-});
